4aa5b847fe0dcb042335e58f95b1f3e3e7ed920b
DiamondYuan <admin@diamondyuan.com>
2019-12-04 15:26:22 +0800

    :white_check_mark: update test for useLoading

--- src/useLoading/__tests__/index.test.ts
+++ src/useLoading/__tests__/index.test.ts
@@ -3,26 +3,46 @@ import { useState } from 'react';
 import { renderHook, act } from '@testing-library/react-hooks';
 import useLoading from '../index';
 
+const MagicErrorNumber = 42;
+
 describe('useLoading', () => {
   it('should be defined', () => {
     expect(useLoading).toBeDefined();
   });
 
-  const setUp = (successCallback: Function) =>
+  const setUp = ({
+    successCallback,
+    errorCallBack,
+  }: {
+    successCallback?: Function;
+    errorCallBack?: Function;
+  }) =>
     renderHook(() => {
       const [count, setCount] = useState(0);
       const add = () => setCount(r => r + 1);
 
       const onSuccess = (result: number, params: [number]) => {
-        successCallback(count, result, params);
+        if (successCallback) {
+          successCallback(count, result, params);
+        }
+      };
+
+      const onError = (error: Error, params: [number]) => {
+        if (errorCallBack) {
+          errorCallBack(count, error, params);
+        }
       };
 
       const useLoadingResponse = useLoading(
         async (arg: number) => {
+          if (arg === MagicErrorNumber) {
+            throw new Error(`${MagicErrorNumber}`);
+          }
           return arg + count;
         },
         {
           onSuccess,
+          onError,
         },
       );
 
@@ -36,7 +56,7 @@ describe('useLoading', () => {
   it('SuccessCallback should be called with latest state', async () => {
     const successCallback = jest.fn();
 
-    let hook = setUp(successCallback);
+    let hook = setUp({ successCallback });
 
     act(() => {
       hook.result.current.add();
@@ -49,10 +69,26 @@ describe('useLoading', () => {
     expect(successCallback).lastCalledWith(1, 11, [10]);
   });
 
+  it('ErrorCallBack should be called with latest state', async () => {
+    const errorCallBack = jest.fn();
+
+    let hook = setUp({ errorCallBack });
+
+    act(() => {
+      hook.result.current.add();
+    });
+    act(() => {
+      hook.result.current.useLoadingResponse.run(MagicErrorNumber);
+    });
+    expect(hook.result.current.count).toEqual(1);
+    await hook.waitForNextUpdate();
+    expect(errorCallBack).lastCalledWith(1, new Error(`${MagicErrorNumber}`), [MagicErrorNumber]);
+  });
+
   it('run and cancel should never change', async () => {
     const successCallback = jest.fn();
 
-    let hook = setUp(successCallback);
+    let hook = setUp({ successCallback });
     let run = hook.result.current.useLoadingResponse.run;
     let cancel = hook.result.current.useLoadingResponse.cancel;
     act(() => {
