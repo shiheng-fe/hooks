38ccd767ee0809857e6731606efdbe020004c010
DiamondYuan <admin@diamondyuan.com>
2019-12-04 15:14:49 +0800

    :white_check_mark: add test for useLoading

--- src/useLoading/__tests__/index.test.ts
+++ src/useLoading/__tests__/index.test.ts
@@ -8,21 +8,21 @@ describe('useLoading', () => {
     expect(useLoading).toBeDefined();
   });
 
-  it('SuccessCallback should be called with latest state', async () => {
-    const successCallback = jest.fn();
-
-    let hook = renderHook(() => {
+  const setUp = (successCallback: Function) =>
+    renderHook(() => {
       const [count, setCount] = useState(0);
       const add = () => setCount(r => r + 1);
 
+      const onSuccess = (result: number, params: [number]) => {
+        successCallback(count, result, params);
+      };
+
       const useLoadingResponse = useLoading(
         async (arg: number) => {
-          return arg;
+          return arg + count;
         },
         {
-          onSuccess(result, params) {
-            successCallback(count, result, params);
-          },
+          onSuccess,
         },
       );
 
@@ -33,15 +33,32 @@ describe('useLoading', () => {
       };
     });
 
+  it('SuccessCallback should be called with latest state', async () => {
+    const successCallback = jest.fn();
+
+    let hook = setUp(successCallback);
+
     act(() => {
       hook.result.current.add();
     });
-
     act(() => {
       hook.result.current.useLoadingResponse.run(10);
     });
     expect(hook.result.current.count).toEqual(1);
     await hook.waitForNextUpdate();
-    expect(successCallback).lastCalledWith(1, 10, [10]);
+    expect(successCallback).lastCalledWith(1, 11, [10]);
+  });
+
+  it('run and cancel should never change', async () => {
+    const successCallback = jest.fn();
+
+    let hook = setUp(successCallback);
+    let run = hook.result.current.useLoadingResponse.run;
+    let cancel = hook.result.current.useLoadingResponse.cancel;
+    act(() => {
+      hook.result.current.add();
+    });
+    expect(run === hook.result.current.useLoadingResponse.run).toBeTruthy();
+    expect(cancel === hook.result.current.useLoadingResponse.cancel).toBeTruthy();
   });
 });
