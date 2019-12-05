d0084307dc76f23cb8d63c42c3d0164d5dab8052
DiamondYuan <admin@diamondyuan.com>
2019-12-05 12:04:00 +0800

    :sparkles: set onSuccess and onError Result to state

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,7 @@ export type ArgsType<T> = T extends (...args: infer U) => any ? U : never;
 export function delay(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export type PromiseFunction = (...args: any[]) => Promise<any>;
+
+export type PromiseReturnType<T extends PromiseFunction> = Unpack<ReturnType<T>>;
