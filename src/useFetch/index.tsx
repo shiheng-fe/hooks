ed61e779bb669bdb1e78afe8427aa704ada61c59
DiamondYuan <admin@diamondyuan.com>
2019-12-05 12:04:00 +0800

    :sparkles: set onSuccess and onError Result to state

--- src/useFetch/index.tsx
+++ src/useFetch/index.tsx
@@ -1,36 +1,45 @@
 import { useState, useEffect, DependencyList } from 'react';
-import { Unpack } from '../utils';
-import useLoading, { IUseLoadingOption } from '../useLoading';
-
-export interface IUseFetchOptions<T extends (...args: any[]) => Promise<any>>
-  extends IUseLoadingOption<T> {
-  auto?: boolean;
-}
+import { Unpack, PromiseFunction, ArgsType, PromiseReturnType } from '../utils';
+import useLoading from '../useLoading';
 
 export interface IUseFetchState<D> {
   data?: D;
   error?: Error;
 }
 
-export default function useFetch<T extends (...args: any[]) => Promise<any>>(
+export interface IUseFetchOptions<T extends PromiseFunction> {
+  initialState?: IUseFetchState<PromiseReturnType<T>>;
+  onSuccess?: (
+    result: PromiseReturnType<T>,
+    params: ArgsType<T>,
+  ) => void | IUseFetchState<PromiseReturnType<T>>;
+  onError?: (error: Error, params: ArgsType<T>) => void | IUseFetchState<PromiseReturnType<T>>;
+  auto?: boolean;
+}
+
+export default function useFetch<T extends PromiseFunction>(
   func: T,
   deps: DependencyList = [],
-  { auto = true, onError, onSuccess }: IUseFetchOptions<T> = {},
+  { auto = true, onError, onSuccess, initialState }: IUseFetchOptions<T> = {},
 ) {
-  const [state, setState] = useState<IUseFetchState<Unpack<ReturnType<T>>>>({});
+  const [state, setState] = useState<IUseFetchState<Unpack<ReturnType<T>>>>(
+    () => initialState || {},
+  );
 
   const { loading, run, cancel, params } = useLoading(func, {
     onSuccess: (data, params) => {
-      setState(s => ({ ...s, data }));
       if (onSuccess) {
-        onSuccess(data, params);
+        setState({ data, ...onSuccess(data, params) });
+        return;
       }
+      setState({ data });
     },
     onError: (error, params) => {
-      setState(s => ({ ...s, error }));
       if (onError) {
-        onError(error, params);
+        setState(s => ({ ...s, error, ...onError(error, params) }));
+        return;
       }
+      setState(s => ({ ...s, error }));
     },
   });
 
