06eef923aefe10e05dac74d09bab70573c006531
DiamondYuan <admin@diamondyuan.com>
2019-12-09 09:51:04 +0800

    :bug: export useFetch

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import useSelections from './useSelections';
 import useLocalStorageState from './useLocalStorageState';
 import useLoading from './useLoading';
 import useEventCallback from './useEventCallback';
+import useFetch from './useFetch';
 
-export { useModal, useSelections, useLocalStorageState, useLoading, useEventCallback };
+export { useModal, useSelections, useLocalStorageState, useLoading, useEventCallback, useFetch };
