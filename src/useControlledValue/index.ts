0609a0c0b89867ccc7837ef2fc80bf4096442453
asakura <shihang.cao@shihengtech.com>
2021-12-03 17:45:23 +0800

    fix: :memo: add doc: useControlledValue

--- src/useControlledValue/index.ts
+++ src/useControlledValue/index.ts
@@ -2,17 +2,24 @@ import { useState } from 'react';
 import useImmutableFn from '../useImmutableFn';
 
 export type OptionsType<T> = {
+  /**
+   * @default '-'
+   * @description 默认值，优先级顺序 props.value > defaultValue > props.defaultValue
+   */
   defaultValue?: T;
   /**
    * @default 'defaultValue'
+   * @description 默认值所使用的属性名
    */
   defaultValueName?: string;
   /**
    * @default 'value'
+   * @description 值所使用的属性名
    */
   valueName?: string;
   /**
    * @default 'onChange'
+   * @description 值发生变化时调用的函数名
    */
   onChangeName?: string;
 };
